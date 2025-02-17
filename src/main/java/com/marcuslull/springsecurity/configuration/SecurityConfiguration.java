5979adc0b8ea5b4277826b7755beb55a4e7f2c61
Marcus Lull <marcuslull@gmail.com>
2025-02-17 15:54:44 -0800

    Basic Security Config
    Adding a basic security config to get started

---
+++ src/main/java/com/marcuslull/springsecurity/configuration/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.marcuslull.springsecurity.configuration;
+
+import com.marcuslull.springsecurity.model.SecurityUser;
+import com.marcuslull.springsecurity.repository.AppUserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    private final AppUserRepository appUserRepository;
+
+    public SecurityConfiguration(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> new SecurityUser(appUserRepository.findByEmail(username).orElseThrow());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .build();
+    }
+
+}
